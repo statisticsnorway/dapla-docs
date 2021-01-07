3d710993c99bdb1faf1456fce68bddcc48f82ec0
Kenneth Leine Schulstad <ks@udp.no>
2021-01-07 09:00:03 +0100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -18,11 +18,12 @@ git worktree add -B gh-pages public origin/gh-pages
 echo "Removing existing files"
 rm -rf public/*
 
+# Use hugo -D if you want to include drafts
 echo "Generating site"
-hugo
+hugo 
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
+echo "Pushing to github"
 git push --all
